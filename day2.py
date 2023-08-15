bdd94b9c4ba7dfd720cec28b209786ef82d62dac
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-15 17:44:11 -0500

    refactored code

--- day2.py
+++ day2.py
@@ -5,14 +5,14 @@
 # This total amount is the printed out on the console 
 # which calculates the amount owed by each individual. 
 
-bill_amount = input("How much is the bill? ")
+bill_amount = float(input("How much is the bill? "))
 
-tip = input("How much would you like to tip? (12%, 20%, or 25%) ")
+tip = int(input("How much would you like to tip? (12%, 20%, or 25%) "))
 
-people_share = input(
-    "Among how many people would you like to split the bill with? ")
+people_share = int(input(
+    "Among how many people would you like to split the bill with? "))
 
-total = ((float(bill_amount) / int(people_share)) *
-         (int(tip) / 100)) + (float(bill_amount) / int(people_share))
+total = (bill_amount / people_share *
+         (tip / 100)) + (bill_amount / people_share)
 
 print("{:.2f}".format(total))
