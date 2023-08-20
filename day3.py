0da1897f7f61aedb8a0a870df98d40f2f4452bbd
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-20 11:40:10 -0500

    added a leap year program

--- day3.py
+++ day3.py
@@ -1,16 +1,31 @@
-# This is a small program calculating your BMI.
+# # This is a small program calculating your BMI.
 
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
-bmi = round(weight / (height ** 2))
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+# bmi = round(weight / (height ** 2))
 
-if bmi < 18.5: 
-    print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi < 25:
-    print(f"Your BMI is {bmi}, you are normal weight.")
-elif bmi < 30:
-    print(f"Your BMI is {bmi}, you are slightly overweight.") 
-elif bmi < 35:
-    print(f"Your BMI is {bmi}, you are obese!")
-else: 
-    print(f"Your BMI is {bmi}, you are clinically obese!")
+# if bmi < 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi < 25:
+#     print(f"Your BMI is {bmi}, you are normal weight.")
+# elif bmi < 30:
+#     print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi < 35:
+#     print(f"Your BMI is {bmi}, you are obese!")
+# else:
+#     print(f"Your BMI is {bmi}, you are clinically obese!")
+
+
+# This program determines whether a year is a leap year
+year = int(input("What year were you born? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("It is a leap year! ")
+        else:
+            print("Not a leap year!")
+    else:
+        print("It is a leap year! ")
+else:
+    print("Not a leap year! ")
