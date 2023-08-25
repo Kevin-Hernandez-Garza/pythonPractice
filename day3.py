77662ea5f99cfb719dd3f55945defd2e92125785
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-24 22:54:28 -0500

    adding pizza order program

--- day3.py
+++ day3.py
@@ -29,3 +29,27 @@
 #         print("It is a leap year! ")
 # else:
 #     print("Not a leap year! ")
+
+
+# The following program calculates the user's pizza order price
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+if size == 'S' or size == 's':
+    pizza_prize = 15
+    if add_pepperoni == 'y' or add_pepperoni == 'Y':
+        pizza_prize += 2
+if size == 'M' or size == 'm':
+    pizza_prize = 20
+    if add_pepperoni == 'Y' or add_pepperoni == 'y':
+        pizza_prize += 3
+if size == 'L' or size == 'l':
+    pizza_prize = 25
+    if add_pepperoni == 'Y' or add_pepperoni == 'y':
+        pizza_prize += 3
+if extra_cheese == 'y' or extra_cheese == 'Y':
+    pizza_prize += 1
+
+print(f"Your final bill is: ${pizza_prize}")
