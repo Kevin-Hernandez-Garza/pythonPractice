34c70be27fafca61d150ca9d0c22331f6f0cc768
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-31 22:10:53 -0500

    importing random module and messing around with some methods

---
+++ day4.py
@@ -0,0 +1,11 @@
+# importing the random module
+import random
+
+# random number from 1-10
+random_int = random.randint(1, 10)
+print(random_int)
+
+
+# random  number from 0.0-1.0
+random_float = random.random()
+print(random_float)
