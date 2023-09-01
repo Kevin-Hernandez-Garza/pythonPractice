b04e2d2fb1fe2a7b47c83cf1b66eb091b9bc1ebe
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-31 23:06:28 -0500

    implemented a heads or tails game

--- day2.py
+++ day2.py
@@ -2,9 +2,10 @@
 
 # This program takes in the bill amount, tip percentage, and the number of people being split
 
-# This total amount is the printed out on the console 
-# which calculates the amount owed by each individual. 
+# This total amount is the printed out on the console
+# which calculates the amount owed by each individual.
 
+import random
 bill_amount = float(input("How much is the bill? "))
 
 tip = int(input("How much would you like to tip? (12%, 20%, or 25%) "))
@@ -13,8 +14,17 @@ people_share = int(input(
     "Among how many people would you like to split the bill with? "))
 
 calculation = (bill_amount / people_share *
-         (tip / 100)) + (bill_amount / people_share)
+               (tip / 100)) + (bill_amount / people_share)
 
 total = "{:.2f}".format(calculation)
 
 print(f"Each person should pay the following amount: ${total}")
+
+
+# a heads or tails games, importing the random module
+
+coin_toss = random.randint(0, 1)
+if coin_toss == 1:
+    print('Heads')
+else:
+    print('Tails')
