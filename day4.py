369089daa2ac0bffa1d991ebbcbe21eb3e28bf56
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-09-13 14:41:58 -0500

    implemented random banker roulette program

--- day4.py
+++ day4.py
@@ -21,5 +21,17 @@
 #     print('Tails')
 
 # Banker Roulette Program (below)
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
 
+# storing the length of the list
+length = len(names)
 
+# getting a random number from the total length
+choice = random.randint(0, length)
+
+# printing out the index that was randomly chosen
+print(f'{names[choice]} is going to buy the meal today!')
